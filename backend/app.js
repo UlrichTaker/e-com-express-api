8b029cb5938ba5446269b83c53ba4078b471d7ea
UlrichTaker <52118710+UlrichTaker@users.noreply.github.com>
2023-11-10 10:52:43 +0000

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
+
 const app = express();//Cette ligne crée une instance d'Express en appelant express(). app devient l'instance principale de l'application Express, à travers laquelle vous pouvez configurer des routes et des gestionnaires de requêtes.
-const stuffRoutes = require('./routes/stuff');
-const userRoutes = require('./routes/user');
-const path = require('path');
-
-
-//Connexion à la base de données
-mongoose.connect('mongodb+srv://Abl:a8AttEnpNz8tRk5@firstcluster.jqgu5ws.mongodb.net/?retryWrites=true&w=majority',
-{ useNewUrlParser: true,
-useUnifiedTopology: true })
-.then(() => console.log('Connexion à MongoDB réussie !'))
-.catch(() => console.log('Connexion à MongoDB échouée !'));
-
-
-app.use(express.json());//Express prend toutes les requêtes qui ont comme Content-Type  application/json  et met à disposition leur  body  directement sur l'objet req, Il permet à Express de prendre le corps de la requête JSON et de le transformer en un objet JavaScript disponible sous req.body. Cela facilite le traitement des données JSON dans les requêtes POST
-
-//Middleware CORS: permet l'accès à l'API depuis n'importe quelle origine, définit les en-têtes autorisés, et spécifie les méthodes HTTP autorisées.
-app.use((req, res, next) => {//le middleware ne prend pas d'adresse en premier paramètre, afin de s'appliquer à toutes les routes. Cela permettra à toutes les demandes de toutes les origines d'accéder à votre API
-  res.setHeader('Access-Control-Allow-Origin', '*');//accéder à notre API depuis n'importe quelle origine ( '*' ) ;
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');//ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');//envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
-  next();
+//Cette section définit un middleware global en utilisant app.use. Le middleware est une fonction qui est exécutée pour chaque requête entrante. Dans ce cas, le middleware envoie une réponse JSON avec le message "Votre requête a bien été reçue !" en réponse à toutes les requêtes.
+app.use((req, res) => {
+   res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
-
-app.use('/images', express.static(path.join(__dirname, 'images')));// Cela indique à Express qu'il faut gérer la ressource images de manière statique (un sous-répertoire de notre répertoire de base, __dirname) à chaque fois qu'elle reçoit une requête vers la route /images
-app.use('/api/stuff', stuffRoutes);//utilise les routes définies dans le module stuffRoutes pour gérer les requêtes relatives aux objets "stuff". Les routes de l'API commencent par /api/stuff. Par exemple, une requête GET à /api/stuff sera gérée par stuffRoutes.
-app.use('/api/auth', userRoutes);
 
 module.exports = app;//Cette ligne crée une instance d'Express en appelant express(). app devient l'instance principale de l'application Express, à travers laquelle vous pouvez configurer des routes et des gestionnaires de requêtes.
--- backend/server.js
+++ backend/server.js
@@ -1,53 +1,10 @@
+//Importer le package HTTP natif de Node 
 const http = require('http');
+
 const app = require('./app');
 
-//La fonction normalizePort renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne.Elle tente de normaliser le port sur lequel le serveur écoutera. Elle prend en entrée une valeur val, tente de la convertir en entier, et retourne val si elle ne peut pas être convertie en entier. Sinon, elle retourne le port s'il est supérieur ou égal à zéro, sinon elle retourne false 
-const normalizePort = val => {
-  const port = parseInt(val, 10);
+app.set('port', process.env.PORT || 3000);//Cette ligne configure le serveur en définissant un paramètre nommé 'port' dans l'objet app. Il utilise process.env.PORT pour obtenir le numéro de port à partir des variables d'environnement. Si aucune variable d'environnement PORT n'est définie, le port par défaut est 3000.
+const server = http.createServer(app);//Cette ligne crée un serveur HTTP en utilisant la fonction createServer du module http. Le serveur est configuré pour utiliser l'objet app comme gestionnaire de requêtes. Cela signifie que toutes les requêtes HTTP reçues par le serveur seront gérées par l'application définie dans app.
 
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-};
-// Cette section utilise la fonction normalizePort pour déterminer le port sur lequel le serveur écoutera. Elle vérifie d'abord s'il existe une variable d'environnement PORT et l'utilise, sinon elle utilise le port par défaut 3000. 
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);// configure l'application Express pour utiliser ce port en utilisant app.set('port', port)
+server.listen(process.env.PORT || 3000);//vous configurez le serveur pour qu'il écoute :•	soit la variable d'environnement du port grâce à process.env.PORT : si la plateforme de déploiement propose un port par défaut, c'est celui-ci qu'on écoutera ;•	soit le port 3000.
 
-// La fonction errorHandler est définie pour gérer les erreurs potentielles lors de la configuration du serveur. Elle vérifie si l'erreur est liée à l'appel système "listen" du serveur. En cas d'erreur, elle affiche un message d'erreur spécifique et, dans certains cas, termine le processus Node.js avec process.exit(1).
-const errorHandler = error => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
-
-// Cette ligne crée un serveur HTTP en utilisant le module http et configure l'application Express app comme gestionnaire de requêtes.
-const server = http.createServer(app);
-
-// Ces lignes définissent des gestionnaires d'événements pour le serveur. L'événement 'error' est utilisé pour gérer les erreurs, comme défini dans la fonction errorHandler. L'événement 'listening' est utilisé pour afficher un message indiquant sur quel port le serveur écoute lorsque le serveur démarre.    
-server.on('error', errorHandler);//Cette ligne configure un gestionnaire d'événements pour l'événement 'error' du serveur. Cela signifie que lorsqu'une erreur survient dans le serveur, la fonction errorHandler sera appelée pour gérer cette erreur.
-server.on('listening', () => {//Cette ligne configure un gestionnaire d'événements pour l'événement 'listening' du serveur. Cela signifie que lorsque le serveur commence à écouter les requêtes entrantes sur un port spécifique, la fonction fléchée sera exécutée.
-  const address = server.address();//Cette ligne récupère les informations sur l'adresse sur laquelle le serveur écoute. L'objet address contient des informations telles que l'adresse IP et le port.
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;//Cette ligne crée une chaîne bind qui décrit comment le serveur est lié. Si address est une chaîne (par exemple, lorsqu'il est lié à un pipe), la chaîne "pipe" est ajoutée avant l'adresse. Sinon, la chaîne "port" est ajoutée avant le numéro de port port.
-  console.log('Listening on ' + bind);
-});
-
-// Cette ligne démarre effectivement le serveur en l'écoutant sur le port configuré (port) en utilisant server.listen(port)
-server.listen(port);
